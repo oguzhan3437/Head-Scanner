6d51a5e9571ad97b1256b1a329b999968851c244
oguzhan <ocetin169@gmail.com>
2021-11-25 12:46:08 +0300

    code update

--- app/src/main/java/com/oguzhancetin/goodpostureapp/MainActivity.kt
+++ app/src/main/java/com/oguzhancetin/goodpostureapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.oguzhancetin.goodpostureapp
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.*
-import android.graphics.drawable.Drawable
 import android.media.Image
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -26,14 +25,11 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import android.media.tv.TvContract
 import android.view.View
 import android.widget.ImageView
 import androidx.camera.view.PreviewView
-import androidx.core.graphics.PaintCompat
 import com.bumptech.glide.Glide
 import java.io.ByteArrayOutputStream
-import java.nio.ByteBuffer
 import android.graphics.BitmapFactory
 
 import android.graphics.Bitmap
@@ -274,131 +270,45 @@ class MainActivity : AppCompatActivity() {
     private fun processPose(pose: Pose, canvas: Canvas,paint: Paint,view:View) {
         try {
 
-            val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
-            val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
-
-            val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
-            val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
-
-            val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
-            val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+            var leftSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+            var rightSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+            var rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
+            var leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
 
 
-            val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
-            val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+            val leftSholderLandMarks: Pair<Float,Float> = Pair(leftSholder.position.x,leftSholder.position.y)
+            val rightSholderLandMarks: Pair<Float,Float> = Pair(rightSholder.position.x,rightSholder.position.y)
+            val leftEarLandMarks: Pair<Float,Float> = Pair(leftEar.position.x,leftEar.position.y)
+            val rightEarLandMarks: Pair<Float,Float> = Pair(rightEar.position.x,rightEar.position.y)
 
 
-            val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
-            val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
 
 
-            val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
-            val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
 
-            val leftEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE)
-            val rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE)
-
-
-            val leftEyePosition = leftEye.position
-            val lEyeX = leftEyePosition.x
-            val lEyeY = leftEyePosition.y
-            val rightEyePosition = rightEye.position
-            val rEyeX = rightEyePosition.x
-            val rEyeY = rightEyePosition.y
-
-            val leftShoulderP = leftShoulder.position
-            val lShoulderX = leftShoulderP.x
-            val lShoulderY = leftShoulderP.y
-            val rightSoulderP = rightShoulder.position
-            val rShoulderX = rightSoulderP.x
-            val rShoulderY = rightSoulderP.y
-
-            val leftElbowP = leftElbow.position
-            val lElbowX = leftElbowP.x
-            val lElbowY = leftElbowP.y
-            val rightElbowP = rightElbow.position
-            val rElbowX = rightElbowP.x
-            val rElbowY = rightElbowP.y
-
-            val leftWristP = leftWrist.position
-            val lWristX = leftWristP.x
-            val lWristY = leftWristP.y
-            val rightWristP = rightWrist.position
-            val rWristX = rightWristP.x
-            val rWristY = rightWristP.y
-
-            val leftHipP = leftHip.position
-            val lHipX = leftHipP.x
-            val lHipY = leftHipP.y
-            val rightHipP = rightHip.position
-            val rHipX = rightHipP.x
-            val rHipY = rightHipP.y
-
-            val leftKneeP = leftKnee.position
-            val lKneeX = leftKneeP.x
-            val lKneeY = leftKneeP.y
-            val rightKneeP = rightKnee.position
-            val rKneeX = rightKneeP.x
-            val rKneeY = rightKneeP.y
-
-            val leftAnkleP = leftAnkle.position
-            val lAnkleX = leftAnkleP.x
-            val lAnkleY = leftAnkleP.y
-            val rightAnkleP = rightAnkle.position
-            val rAnkleX = rightAnkleP.x
-            val rAnkleY = rightAnkleP.y
-
-
-            DisplayAll(
-                lShoulderX, lShoulderY, rShoulderX, rShoulderY,
-                lElbowX, lElbowY, rElbowX, rElbowY,
-                lWristX, lWristY, rWristX, rWristY,
-                lHipX, lHipY, rHipX, rHipY,
-                lKneeX, lKneeY, rKneeX, rKneeY,
-                lAnkleX, lAnkleY, rAnkleX, rAnkleY, canvas,paint,view ,lEyeX, lEyeY, rEyeX, rEyeY
-            )
+            DisplayAll(leftSholderLandMarks,rightSholderLandMarks,leftEarLandMarks,rightEarLandMarks,canvas,paint,view)
         } catch (e: Exception) {
             Toast.makeText(this@MainActivity, "Pose Landmarks failed", Toast.LENGTH_SHORT).show()
         }
     }
-    private fun DisplayAll(
-        lShoulderX: Float, lShoulderY: Float, rShoulderX: Float, rShoulderY: Float,
-        lElbowX: Float, lElbowY: Float, rElbowX: Float, rElbowY: Float,
-        lWristX: Float, lWristY: Float, rWristX: Float, rWristY: Float,
-        lHipX: Float, lHipY: Float, rHipX: Float, rHipY: Float,
-        lKneeX: Float, lKneeY: Float, rKneeX: Float, rKneeY: Float,
-        lAnkleX: Float, lAnkleY: Float, rAnkleX: Float, rAnkleY: Float, canvas: Canvas,paint:Paint,view:View,
-        lEyeX: Float, lEyeY: Float, rEyeX: Float, rEyeY: Float
-
-    ) {
+    private fun   DisplayAll(
+        leftSholderLandMarks:Pair<Float,Float>,
+        rightSholderLandMarks:Pair<Float,Float>,
+        leftEarLandMarks:Pair<Float,Float>,
+        rightEarLandMarks:Pair<Float,Float>,
+        canvas:Canvas,
+        paint:Paint,
+        view:View) {
 
 
 
-        canvas.drawLine(lEyeX, lEyeY, rEyeX, rEyeY, paint)
+        canvas.drawLine(
+            leftSholderLandMarks.first,leftSholderLandMarks.second,
+            leftEarLandMarks.first,leftEarLandMarks.second,
+            paint
+        )
 
-        canvas.drawLine(lShoulderX, lShoulderY, rShoulderX, rShoulderY, paint)
 
-        canvas.drawLine(rShoulderX, rShoulderY, rElbowX, rElbowY, paint)
 
-        canvas.drawLine(rElbowX, rElbowY, rWristX, rWristY, paint)
-
-        canvas.drawLine(lShoulderX, lShoulderY, lElbowX, lElbowY, paint)
-
-        canvas.drawLine(lElbowX, lElbowY, lWristX, lWristY, paint)
-
-        canvas.drawLine(rShoulderX, rShoulderY, rHipX, rHipY, paint)
-
-        canvas.drawLine(lShoulderX, lShoulderY, lHipX, lHipY, paint)
-
-        canvas.drawLine(lHipX, lHipY, rHipX, rHipY, paint)
-
-        canvas.drawLine(rHipX, rHipY, rKneeX, rKneeY, paint)
-
-        canvas.drawLine(lHipX, lHipY, lKneeX, lKneeY, paint)
-
-        canvas.drawLine(rKneeX, rKneeY, rAnkleX, rAnkleY, paint)
-
-        canvas.drawLine(lKneeX, lKneeY, lAnkleX, lAnkleY, paint)
         view.invalidate()
 
 
