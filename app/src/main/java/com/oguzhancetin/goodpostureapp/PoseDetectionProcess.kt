65442ca34186ebdfd067d5fdc6d43ebe6bba5af1
oguzhan <ocetin169@gmail.com>
2022-02-12 01:16:20 +0300

    fix getAngle method

--- app/src/main/java/com/oguzhancetin/goodpostureapp/PoseDetectionProcess.kt
+++ app/src/main/java/com/oguzhancetin/goodpostureapp/PoseDetectionProcess.kt
@@ -32,52 +32,7 @@ class PoseDetectionProcess(
 
         poseDetector.process(InputImage.fromBitmap(bitmap, 0))
             .addOnSuccessListener { pose ->
-                try {
-                    val leftSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
-                    val rightSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
-                    val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
-                    val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
-
-
-                    val leftSholderLandMarks: Pair<Float, Float> =
-                        Pair(leftSholder.position.x, leftSholder.position.y)
-                    val rightSholderLandMarks: Pair<Float, Float> =
-                        Pair(rightSholder.position.x, rightSholder.position.y)
-                    val leftEarLandMarks: Pair<Float, Float> = Pair(leftEar.position.x, leftEar.position.y)
-                    val rightEarLandMarks: Pair<Float, Float> =
-                        Pair(rightEar.position.x, rightEar.position.y)
-
-
-
-                    val pairEar = Pair(leftEarLandMarks.first,leftEarLandMarks.second)
-                    val pairShoulder = Pair(leftSholderLandMarks.first,leftSholderLandMarks.second)
-                    val pairIntersection = Pair(leftSholderLandMarks.first,leftEarLandMarks.second)
-
-
-
-
-                    canvas.drawLine(
-                        leftSholderLandMarks.first, leftSholderLandMarks.second,
-                        leftEarLandMarks.first, leftEarLandMarks.second,
-                        paint
-                    )
-                    canvas.drawPoint(
-                        leftSholderLandMarks.first,leftEarLandMarks.second,
-                        paint
-                    )
-                    canvas.drawLine(
-                        leftSholderLandMarks.first, leftEarLandMarks.second,
-                        leftSholderLandMarks.first, leftSholderLandMarks.second,
-                        paint
-                    )
-                    val angle = getAngle(pairIntersection,pairShoulder,pairEar)
-                    canvas.drawText("angle : "+(90-angle),200f,50f,paint)
-
-
-
-                }catch (e:Exception){
-                    Log.e("exception Pose",e.stackTraceToString())
-                }
+                displayAll(pose)
 
                 resultCallback.invoke(ProcessResult.ProcessSucces("Başarılı"))
 
@@ -89,17 +44,62 @@ class PoseDetectionProcess(
 
     }
 
-    private fun DisplayAll(pose: Pose) {
+    private fun displayAll(pose: Pose) {
+
+        try {
+            val leftSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)!!
+            val rightSholder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)!!
+            val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)!!
+            val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)!!
 
 
+            val leftSholderLandMarks: Pair<Float, Float> =
+                Pair(leftSholder.position.x, leftSholder.position.y)
+            val rightSholderLandMarks: Pair<Float, Float> =
+                Pair(rightSholder.position.x, rightSholder.position.y)
+            val leftEarLandMarks: Pair<Float, Float> = Pair(leftEar.position.x, leftEar.position.y)
+            val rightEarLandMarks: Pair<Float, Float> =
+                Pair(rightEar.position.x, rightEar.position.y)
+
+
+
+            val pairEar = Pair(leftEarLandMarks.first,leftEarLandMarks.second)
+            val pairShoulder = Pair(leftSholderLandMarks.first,leftSholderLandMarks.second)
+            val pairIntersection = Pair(leftSholderLandMarks.first,leftEarLandMarks.second)
+
+            canvas.drawLine(
+                leftSholderLandMarks.first, leftSholderLandMarks.second,
+                leftEarLandMarks.first, leftEarLandMarks.second,
+                paint
+            )
+            canvas.drawPoint(
+                leftSholderLandMarks.first,leftEarLandMarks.second,
+                paint
+            )
+            canvas.drawLine(
+                leftSholderLandMarks.first, leftEarLandMarks.second,
+                leftSholderLandMarks.first, leftSholderLandMarks.second,
+                paint
+            )
+            val angle = getAngle(pairIntersection,leftSholder,leftEar)
+
+
+
+            canvas.drawText("angle : "+angle,200f,50f,paint)
+
+
+
+        }catch (e:Exception){
+            Log.e("exception Pose",e.stackTraceToString())
+        }
 
     }
-    private fun getAngle(firstPoint: Pair<Float, Float>, midPoint: Pair<Float, Float>, lastPoint: Pair<Float, Float>): Double {
+    private fun getAngle(pair: Pair<Float,Float>, midPoint: PoseLandmark, lastPoint: PoseLandmark): Double {
         var result = Math.toDegrees(
-            (atan2(lastPoint.second - midPoint.second,
-                lastPoint.first - midPoint.second)
-                    - atan2(firstPoint.second - midPoint.second,
-                firstPoint.first- midPoint.first)).toDouble()
+            (atan2(lastPoint.position.y - midPoint.position.y,
+                lastPoint.position.x - midPoint.position.x)
+                    - atan2(pair.second - midPoint.getPosition().y,
+                pair.first - midPoint.position.x)).toDouble()
         )
         result = Math.abs(result) // Angle should never be negative
         if (result > 180) {
--- app/src/main/java/com/oguzhancetin/goodpostureapp/fragment/MainFragment.kt
+++ app/src/main/java/com/oguzhancetin/goodpostureapp/fragment/MainFragment.kt
@@ -77,11 +77,11 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
                 findNavController().navigate(direction)
             }
         }
-        /*binding.buttonGotoExercises.setOnClickListener {
+        binding.buttonGotoExercises.setOnClickListener {
             MainFragmentDirections.actionMainFragmentToExercisesFragment().also {
                 findNavController().navigate(it)
             }
-        }*/
+        }
 
         checkPermissionsOk()
 
